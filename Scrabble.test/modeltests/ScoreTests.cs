72841b958b3e394b01c4c5cfc72299ec3f4886f1
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-07-14 09:31:33 -0700

    2nd commit

---
+++ Scrabble.test/modeltests/ScoreTests.cs
@@ -0,0 +1,21 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  {
+
+
+    // [TestMethod]
+    //   public void ItemConstructor_CreatesInstanceOfItem_Item()
+    // {
+    //   Item newItem = new Item("test"); //We pass in "test" as an argument here.
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // }
+
+  }
+}
---
+++ Scrabble/program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace //
+{
+  public class //
+  {
+    public static void Main()
+    {
+
+    
+    }
+  }
